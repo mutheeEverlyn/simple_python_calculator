cb8693e54b60c838de207f21b950cac3d3b7ce11
mutheeEverlyn <muthee.14952@students.kyu.ac.ke>
2025-03-12 12:29:26 +0300

    changes

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+operator=input("Enter operator: ")
+
+if operator=="+":
+    print(f"{num1}{operator}{num2} = {num1+num2}")
+elif operator=="-":
+    print(f"{num1}{operator}{num2} = {num1-num2}")
+elif operator=="*":
+    print(f"{num1}{operator}{num2} = {num1*num2}")
+elif operator=="/":
+    print(f"{num1}{operator}{num2} = {num1/num2}")
+else:
+    print("Invalid operator")    
