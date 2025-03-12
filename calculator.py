97e8e43f541176f574bc5a3963556eb9c6149a0e
mutheeEverlyn <muthee.14952@students.kyu.ac.ke>
2025-03-12 12:31:38 +0300

    format output for calculator operations to include spaces around operators

--- calculator.py
+++ calculator.py
@@ -3,12 +3,12 @@ num2=float(input("Enter second number: "))
 operator=input("Enter operator: ")
 
 if operator=="+":
-    print(f"{num1}{operator}{num2} = {num1+num2}")
+    print(f"{num1} {operator} {num2} = {num1+num2}")
 elif operator=="-":
-    print(f"{num1}{operator}{num2} = {num1-num2}")
+    print(f"{num1} {operator} {num2} = {num1-num2}")
 elif operator=="*":
-    print(f"{num1}{operator}{num2} = {num1*num2}")
+    print(f"{num1} {operator} {num2} = {num1*num2}")
 elif operator=="/":
-    print(f"{num1}{operator}{num2} = {num1/num2}")
+    print(f"{num1} {operator} {num2} = {num1/num2}")
 else:
     print("Invalid operator")    
